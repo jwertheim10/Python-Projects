5c0c0a109ca6c271f696bf4092ac7b1ef695fdc1
Josh Wertheim <jwertheim10@gmail.com>
2025-02-21 11:40:59 -0500

    Extracted function out and added 2 unit tests

--- python_high_level/app_name/views.py
+++ python_high_level/app_name/views.py
@@ -1,15 +1,17 @@
-from django.urls import path
 from django.shortcuts import HttpResponse, render
-from . import urls
+from . import sum_multiple_extracted
 
-def index(request):
+# This function returns hello world on the webpage
+def index(request): 
     return HttpResponse("Hello, world")
 
+# This function adds two numbers that are passed into the url
 def add(request, num1, num2):
     sum = num1 + num2
     return HttpResponse(f"The sum of {num1} and {num2} is {sum}.")
 
-def operation(request):
+# This function takes a string and depending on the string applies one of +-*/ to the two numbers
+def operation(request): 
     action = request.GET.get('action')  # For str: action
     num1 = int(request.GET.get('num1'))  # For int: num1
     num2 = int(request.GET.get('num2'))  # For int: num2
@@ -26,14 +28,8 @@ def operation(request):
         result = 'operator not found, please use one of: add, sub, mult, or div'
     return HttpResponse(f"The result of {action}ing {num1} and {num2} is {result}.")
 
-def sum_multiples(request):
+# Here we get the two numbers passed into the url and pass it onto a helped method
+def sum_multiples(request): 
     num1 = int(request.GET.get('num1'))  # For int: num1
     num2 = int(request.GET.get('num2'))  # For int: num2
-    if num1 > num2:
-        return HttpResponse('Your second number must be greater than or equal to your first number')
-    result = 0
-    i = num1
-    while i <= num2:
-        result += i
-        i += num1
-    return HttpResponse(f"The sum of all multiples of {num1} below {num2} is {result}")
+    return HttpResponse(sum_multiple_extracted.main(num1, num2))
